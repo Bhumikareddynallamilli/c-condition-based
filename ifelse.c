f18700f8e740cce66f14000c50eb6d69179f988e
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2021-12-30 15:08:15 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("enter your age");
+	scanf("%d",&age);//6
+	if(age>18) //6>18
+	{
+		printf("you can vote");
+	}
+	else
+	{
+		printf("you cannot vote");
+	}
+}
